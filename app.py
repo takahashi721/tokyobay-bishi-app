7906d7737b302d6461b67bf9f7f1d414fe317b7a
takahashi721 <michiko3@wg7.so-net.ne.jp>
2026-03-13 23:08:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-
 def index():
-    
-    date = request.args.get("date")
-
-    return render_template("index.html", date=date)
+date = request.args.get("date")
+return render_template("index.html", date=date)
 
 if __name__ == "__main__":
-
-    app.run(host="0.0.0.0", port=10000)
-
+app.run(host="0.0.0.0", port=10000)
